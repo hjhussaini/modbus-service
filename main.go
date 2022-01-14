e3801f7a117a1e1a63136c49cd674f708241e802
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2022-01-14 21:39:54 +0330

    main()
    Implement HTTP server

--- main.go
+++ main.go
@@ -1,14 +1,50 @@
 package main
 
 import (
+    "context"
+    "log"
+    "net/http"
     "os"
     "os/signal"
     "syscall"
+    "time"
 )
 
 func main() {
-    stop := make(chan os.Signal)
-    signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+    server := &http.Server{
+        Addr:   ":3128",
+    }
 
-    <- stop
+    errs := make(chan error, 2)
+    go func() {
+        if err := server.ListenAndServe(); err != nil {
+            errs <-err
+
+            return
+        }
+        errs <-nil
+    }()
+    log.Printf("Started HTTP server on \n")
+
+    go func() {
+        stop := make(chan os.Signal, 1)
+        signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+        <-stop
+
+        timeout, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+        defer cancel()
+        if err := server.Shutdown(timeout); err != nil {
+            errs <-err
+
+            return
+        }
+
+        errs <-timeout.Err()
+    }()
+
+    if err := <-errs; err != nil {
+        log.Fatalf("Failed to shut server down: %v", err)
+    }
+    log.Println("Stopped HTTP server")
 }
