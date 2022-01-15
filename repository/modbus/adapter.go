0601bc42ffd5d41cea5759d9556f53ea19c9b449
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2022-01-15 20:41:27 +0330

    modbus.Adapter
    Declare method Connect()
    Declare method Close()

--- repository/modbus/adapter.go
+++ repository/modbus/adapter.go
@@ -1,7 +1,25 @@
 package modbus
 
+import (
+    "github.com/goburrow/modbus"
+)
+
 type Adapter interface {
+    Connect(protocol string, address string) error
+    Close() error
 }
 
 type ModbusAdapter struct {
+    rtuHandler  modbus.RTUClientHandler
+    tcpHandler  modbus.TCPClientHandler
+    client      modbus.Client
+}
+
+func (adapter *Modpackage) Connect(protocol string, address string) error {
+
+    return nil
+}
+
+func (adapter *ModbusAdapter) Close() error {
+    return nil
 }
