b18b53113b7e6b509e44d6d5590a3b530436f39f
Amir Hussain Haji Hussaini <hajihussaini@afrabon.com>
2022-01-15 20:51:07 +0330

    modbus.ModbusAdapter
    Implement method rtuConnect()

--- repository/modbus/adapter.go
+++ repository/modbus/adapter.go
@@ -20,6 +20,23 @@ func (adapter *Modpackage) Connect(protocol string, address string) error {
     return nil
 }
 
+func (adapter *ModbusAdapter) rtuConnect(address string) error {
+    adapter.rtuHandler = modbus.NewRTUClientHandler(address)
+    adapter.rtuHandler.Timeout = 5 * time.Second
+    adapter.rtuHandler.BaudRate = 115200
+    adapter.rtuHandler.DataBits = 8
+    adapter.rtuHandler.Parity = "N"
+    adapter.rtuHandler.StopBits = 1
+
+    if err := adapter.rtuHandler.Connect(); err != nil {
+        return err
+    }
+
+    adapter.client = modbus.NewClient(adapter.rtuHandler)
+
+    return nil
+}
+
 func (adapter *ModbusAdapter) Close() error {
     return nil
 }
